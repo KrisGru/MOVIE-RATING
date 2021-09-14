d409e6f82e084994258999633d2ddc0207bb5688
Krzysztof Gruszczyński <k.gruszczynski1002@gmail.com>
2021-09-14 15:48:06 +0200

    commit

---
+++ src/components/Modal.js
@@ -0,0 +1,12 @@
+import '../css/modal.scss';
+
+const Modal = (props) => {
+  return(
+    <div className="modal">
+      <p className="p">lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum</p>
+    </div>
+
+  )
+}
+
+export default Modal;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,21 @@
 import '../css/pulpitAndMovie.scss';
+import Modal from './Modal';
 
 const Movie = (props) => {
-
+console.log(props.movie)
   return (
-      <section className="movies" id={props.id} onClick={() => props.handleClickShowModal(props.id) }>
-        <img className="movies__image" src={props.image} alt="movie's poster"/>
-        <h2 className="movies__title">{props.title}</h2>
-        <p className="movies__category">{props.category}</p>
-        <p className="movies__rating">{props.rating}</p>
-        {props.showModalID===props.id  && props.showModalClick ? <div>{props.id}</div> : null}
-      </section>
+    <>
+    <section className="movies" id={props.id} onClick={() => props.handleClickShowModal(props.id) }>
+      <img className="movies__image" src={props.image} alt="movie's poster"/>
+      <h2 className="movies__title">{props.title}</h2>
+      <p className="movies__category">{props.category}</p>
+      <p className="movies__rating">{props.rating}</p>
+    </section>
+    {props.showModalID===props.id  && props.showModalClick ?
+    <Modal /> : null}
+    </>
+
+
     )
   }
 
