e741e6996d67d827d64f0bf3d909aeade24f8771
Krzysztof Gruszczynski <k.gruszczynski1002@gmail.com>
2022-01-04 12:20:06 +0100

    update old project

--- src/components/App.js
+++ src/components/App.js
@@ -7,32 +7,29 @@ import Footer from './Footer';
 const titles = ["orange is the new black", "peaky blinders", "vikings", "lucifer", "how i met your mother", "american pie"]
 
 const App = () => {
-  const [ input, setInput] = useState("")
   const [ listMovies, setListMovies] = useState([])
+  //to know which component I clicked
   const [ showModalClick, setShowModalClick] = useState(false)
   const [ showModalID, setShowModalID] = useState("")
 
 // Handle Fetch
   const handleFetch = (title) => {
-  fetch("https://www.omdbapi.com/?t="+ title +"&plot=full&apikey=b6fe8a66")
-  .then(response => response)
-  .then(response => response.json())
-  .then(title => {
-    setListMovies(listMovies => [title, ...listMovies])
+    fetch("https://www.omdbapi.com/?t="+ title +"&plot=full&apikey=b6fe8a66")
+    .then(response => response)
+    .then(response => response.json())
+    .then(title => {
+      setListMovies(listMovies => [title, ...listMovies])
     })
   }
-
-// fetch on the recommmend movies in website, just for look
+  console.log(listMovies)
+// using fetch to display recommend movies
   useEffect(() => {
     titles.map(title => handleFetch(title))}, [])
 
   return (
     <div className="wrapper">
       <Searching
-        input={input}
-        handleSearchButton={() => {handleFetch(input); setInput('');}}
-        handleInput={(e)=>setInput(e.target.value)}
-        onKeyPress={(event)=> {if(event.key=== 'Enter'){handleFetch(input); setInput('');}}}
+        handleFetch={() => handleFetch()}
         />
       <Nav />
       <ListMovies
--- src/components/Searching.js
+++ src/components/Searching.js
@@ -1,14 +1,19 @@
 import logo from '../assets/logo.jpg';
+import { useState } from 'react';
 import { BsSearch } from 'react-icons/bs'
 
-const Searching = ({ input, handleInput, handleSearchButton, onKeyPress }) => (
+const Searching = ({ handleFetch, handleSearchButton, onKeyPress }) => {
+  const [ input, setInput] = useState("");
+  console.log(input)
+return (
   <div className="searching">
     <a href="./" className="searching__link"><img src={logo} className="searching__image" alt="logo" /></a>
-    <input className="searching__input" type="text" value={input} onChange={handleInput} placeholder="search title" onKeyPress={onKeyPress}/>
-    <button className="searching__btnSearch" onClick={() =>handleSearchButton()}><BsSearch /></button>
+    <input className="searching__input" type="text" value={input} onChange={(e)=>setInput(e.target.value)} placeholder="search title" onKeyPress={(event)=> {if(event.key=== 'Enter'){handleFetch(input); setInput('');}}}/>
+    <button className="searching__btnSearch" onClick={() => {handleFetch(input); setInput('');}}><BsSearch /></button>
     <button className="searching__btn">LOG IN</button>
     <button className="searching__btn">REGISTER</button>
   </div>
 )
+}
 
 export default Searching;
